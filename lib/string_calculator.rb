2a41d6f84b2032adcc5b76b83fb042cfc2c5be2b
Deepak Pandey <ykcir.deep@hotmail.com>
2025-04-21 18:12:28 +0530

    Initialize project and add test for empty string

---
+++ lib/string_calculator.rb
@@ -0,0 +1,5 @@
+module StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+  end
+end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,9 @@
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
+end
