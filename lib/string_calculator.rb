26022bd74dd5d9da70611a55554e338fb9ebb212
Deepak Pandey <ykcir.deep@hotmail.com>
2025-04-21 19:58:14 +0530

    complete code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,28 @@
 module StringCalculator
   def self.add(numbers)
-    numbers_array = numbers.split(/[,\n]/)
-    print("numbers_array: #{numbers_array}\n")
-    numbers = numbers_array.map(&:to_i)
-    numbers.sum
+    return 0 if numbers.to_s.empty?
+
+    delimiter, nums = parse_delimiter(numbers)
+    numbers = nums.split(/#{delimiter}/)
+    negatives = numbers.select { |num| num.to_i < 0 }
+    raise "negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+
+    numbers.sum do |num|
+      n = num.to_i
+      raise "negative numbers not allowed: #{n}" if n < 0
+      n <= 1000 ? n : 0
+    end
+  end
+
+  private
+
+  def self.parse_delimiter(input)
+    if input.start_with?('//')
+      delimiter, nums = input.split("\n", 2)
+      delimiter = delimiter.start_with?('//[') ? Regexp.escape(delimiter[3...-1]) : Regexp.escape(delimiter[2..])
+      [delimiter, nums]
+    else
+      [/[,\n]/, input]
+    end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,6 +13,47 @@ RSpec.describe StringCalculator do
     it 'returns the sum when two number is provided' do
       expect(StringCalculator.add("5, 9")).to eq(14)
     end
+
+    it "given '1\n2,3' & it returns 6" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "given '//;\n1;2' & it returns 3" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "given '-1,2,-3' & it wil raise 'negative numbers not allowed: -1,-3'" do
+      expect { StringCalculator.add("-1,2,-3") }.to raise_error("negative numbers not allowed: -1,-3")
+    end
+
+    it 'returns 6 for //[***]\n1***2***3' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'raises an error with multiple negative numbers' do
+      expect {
+        StringCalculator.add("2,-4,3,-5")
+      }.to raise_error("negative numbers not allowed: -4,-5")
+    end
+  
+    it 'raises an error with negative numbers and custom delimiter' do
+      expect {
+        StringCalculator.add("//[***]\n1***-2***-7")
+      }.to raise_error("negative numbers not allowed: -2,-7")
+    end
+
+    it "handles all numbers > 1000" do
+      expect(StringCalculator.add("1001,2000")).to eq(0)
+    end
+  
+    it "handles custom delimiters with large numbers" do
+      expect(StringCalculator.add("//[;]\n1;1001;2;2000")).to eq(3)
+    end
+  
+    it "handles newlines with large numbers" do
+      expect(StringCalculator.add("1\n1001,2")).to eq(3)
+    end
+  
   end
 
   describe "with extreme inputs" do
@@ -22,8 +63,8 @@ RSpec.describe StringCalculator do
     end
 
     it "handles maximum integer values" do
-      max = 2**(0.size * 8 - 2) - 1 
-      expect(StringCalculator.add("#{max},#{max}")).to eq(max * 2)
+      max = 2**(0.size * 8 - 2) - 1
+      expect(StringCalculator.add("#{max},#{max}")).to eq(0)
     end
 
     it "treats empty elements as zero" do
@@ -34,4 +75,10 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add(" 1 , 2 ")).to eq(3)
     end
   end
+
+  require 'benchmark'
+
+  input = (1..1_000_000).to_a.join(',')
+  time = Benchmark.realtime { StringCalculator.add(input) }
+  puts "Processed 1M numbers in #{time.round(2)}s"
 end
